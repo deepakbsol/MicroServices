84fece4909b12843a75e6c75573b22837db4b00e
deepak <kumar.dpk03@gmail.com>
2023-01-11 09:33:58 +0530

    initial setup of microservices

---
+++ Employee/src/main/java/com/employee/model/Employee.java
@@ -0,0 +1,77 @@
+package com.employee.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="Employee_Dtls")
+public class Employee {
+
+	@Id
+	@Column(name="EMPLOYEE_NUMBER")
+	private int employeeNumber;
+	@Column(name="FIRST_NAME")
+	private String firstName;
+	@Column(name="LAST_NAME")
+	private String lastName;
+	@Column(name="EMAIL")
+	private String email;
+	@Column(name="DEPARTMENT")
+	private String department;
+	@Column(name="SALARY")
+	private String salary;
+	@Override
+	public String toString() {
+		return "Employee [employeeNumber=" + employeeNumber + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", email=" + email + ", department=" + department + ", salary=" + salary + "]";
+	}
+	public int getEmployeeNumber() {
+		return employeeNumber;
+	}
+	public void setEmployeeNumber(int employeeNumber) {
+		this.employeeNumber = employeeNumber;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	
+}
---
+++ Student/src/main/resources/db/migration/V1__Student_Table_Create.sql
@@ -0,0 +1,8 @@
+create table Student_Dtls(
+	USN INT,
+	FIRST_NAME VARCHAR(30),
+	LAST_NAME VARCHAR(30),
+	EMAIL VARCHAR(50),
+	SEM INT,
+	PRIMARY KEY (USN)	
+);
